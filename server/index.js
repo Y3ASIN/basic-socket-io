8f8647b16d914b1c81232c0aa357530193d42ca8
Yeasin Arafat <yeasinarafat@Yeasins-Air.local>
2024-04-25 22:49:35 +0300

    Initial upload of code

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const config = {
+  port: 3001,
+};
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+server.listen(config.port, () => {
+  console.log("Server is running on port 3001...");
+});
